f009a500c7e0bd9e044b7a95630fd398547930c3
An <annguyen160705@gmail.com>
2026-07-14 10:29:15 +0700

    daily

--- Struct/test_3.sv
+++ Struct/test_3.sv
@@ -1,5 +1,25 @@
 module test_3();
 
+typedef struct packed {
+    logic [1:0] addr;
+    logic [1:0] data;
+} packet_t;
+
+packet_t packet_a, packet_b;
+
+
+initial begin
+        
+    packet_a = '{
+        addr : 2'b0,
+        data : 2'b1 
+    };
+
+    packet_b = packet_a;
+
+    packet_b.addr = 2'b11;
+    $display("packet_a %b, packet_b %b", packet_a, packet_b );
+end
 /*
 Exercise 3 - Packet Copy
 
