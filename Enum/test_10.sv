7b204566c98916ed968c81bcfe9f05f33d948840
An <annguyen160705@gmail.com>
2026-07-28 10:04:23 +0700

    daily

--- Enum/test_10.sv
+++ Enum/test_10.sv
@@ -1,4 +1,59 @@
-module test_10();
+module test_10(
+    input logic clk,
+    input logic arstn,
+    output logic OUTPUT
+);
+
+typedef enum logic [2:0] {
+    FETCH    = 3'b000,
+    DECODE = 3'b001,
+    EXECUTE = 3'b010,
+    MEMORY = 3'b011,
+    WRITEBACK = 3'b100
+} state_t;
+
+state_t CUR_STATE;
+state_t NEXT_STATE;
+
+assign OUTPUT = CUR_STATE;
+
+always_ff @(posedge clk or negedge arstn) begin
+    if (!arstn) begin
+        CUR_STATE <= FETCH;
+    end else begin 
+        CUR_STATE <= NEXT_STATE;
+    end
+end
+
+
+always_comb begin
+    case (CUR_STATE)
+        
+        FETCH: begin
+            NEXT_STATE = DECODE;
+        end
+
+        DECODE: begin
+            NEXT_STATE = EXECUTE;
+        end
+
+        EXECUTE: begin
+            NEXT_STATE = MEMORY;
+        end
+
+        MEMORY: begin
+            NEXT_STATE = WRITEBACK;
+        end
+
+        WRITEBACK: begin
+            NEXT_STATE = FETCH;
+        end
+
+        default: begin
+            NEXT_STATE = FETCH;
+        end
+    endcase
+end
 
 /*
 Exercise 10 - CPU Control Unit
--- Enum/test_9.sv
+++ Enum/test_9.sv
@@ -1,4 +1,59 @@
-module test_9();
+module test_9(
+    input logic clk,
+    input logic arstn,
+    output logic OUTPUT
+);
+
+typedef enum logic [2:0] {
+    IDLE    = 3'b000,
+    UP = 3'b001,
+    DOWN = 3'b010,
+    OPEN = 3'b011,
+    CLOSE = 3'b100
+} state_t;
+
+state_t CUR_STATE;
+state_t NEXT_STATE;
+
+assign OUTPUT = CUR_STATE;
+
+always_ff @(posedge clk or negedge arstn) begin
+    if (!arstn) begin
+        CUR_STATE <= ILDE;
+    end else begin 
+        CUR_STATE <= NEXT_STATE;
+    end
+end
+
+always_comb begin
+    case (CUR_STATE)
+        
+        IDLE:begin
+            NEXT_STATE = UP;
+        end
+
+        UP:begin
+            NEXT_STATE = DOWN;
+        end
+
+        DOWN:begin
+            NEXT_STATE = OPEN;
+        end
+
+        OPEN:begin
+            NEXT_STATE = CLOSE;
+        end
+
+        CLOSE:begin
+            NEXT_STATE = IDLE;
+        end
+
+
+        default: begin
+            NEXT_STATE = IDLE;
+        end
+    endcase
+end
 
 /*
 Exercise 9 - Elevator Controller
