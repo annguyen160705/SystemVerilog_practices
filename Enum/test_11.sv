cd320001b11087cba8e7cf4487e3c8b6812c4302
An <annguyen160705@gmail.com>
2026-08-05 23:00:14 +0700

    daily

--- Enum/test_11.sv
+++ Enum/test_11.sv
@@ -1,5 +1,47 @@
-module test_11();
+module test_11(
+    logic clk,
+    logic arstn,
+    logic [1:0] OUTPUT
+);
 
+typedef enum logic [1:0] {
+    POWER_OFF    = 2'b00,
+    POWER_ON = 2'b01,
+    STANDBY = 2'b10,
+    RESET = 2'b11
+} power_state_t;
+
+power_state_t CUR_POWER;
+power_state_t NEXT_POWER;
+
+assign OUTPUT = CUR_POWER;
+
+always_ff @(posedge clk or negedge arstn) begin
+    if (!arstn) begin
+        CUR_POWER <= STANDBY;
+    end else begin
+        CUR_POWER <= NEXT_POWER;
+    end
+end
+
+
+always_comb begin
+    NEXT_POWER = CUR_POWER;
+
+    case (CUR_POWER)
+        
+        POWER_OFF : NEXT_POWER = POWER_ON;
+        POWER_ON : NEXT_POWER = STANDBY;
+        STANDBY : NEXT_POWER = RESET;
+        RESET : NEXT_POWER = POWER_OFF;
+
+        default: begin
+            NEXT_POWER = STANDBY;
+        end
+    endcase
+
+
+end
 /*
 Exercise 11 - Power State
 
