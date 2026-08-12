b60ac4b7a884817a129c9f86a57d0678f5cdc809
An <annguyen160705@gmail.com>
2026-08-12 23:22:43 +0700

    daily

--- Struct/test_14.sv
+++ Struct/test_14.sv
@@ -1,5 +1,35 @@
 module test_14();
 
+typedef struct packed {
+    
+    logic [15:0] address;
+    logic [31:0] data;
+    logic write_enable;
+
+} register_t;
+
+register_t var_1 = '{
+    address : 16'd1234,
+    data : 32'd12344321,
+    write_enable: 1'b1
+};
+
+register_t var_2 = '{
+    default: '0
+};
+
+
+initial begin
+    
+    var_2 = var_1;
+
+    $display("address : %0d \ndata : %0d \nwrite_en: %0d", var_2.address, var_2.data, var_2.write_enable );
+
+
+end
+
+
+
 /*
 Exercise 14 - Structure Assignment
 
