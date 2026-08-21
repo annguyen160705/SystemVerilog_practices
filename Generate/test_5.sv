8f476f2d43f141eaf408518dd4f6437bc70b2d1c
An <annguyen160705@gmail.com>
2026-08-21 23:51:23 +0700

    daily

--- Generate/test_5.sv
+++ Generate/test_5.sv
@@ -1,4 +1,34 @@
-module test_5();
+module decoder 
+#(parameter logic [1:0] MODE = 2'b00)
+(
+    
+);
+
+generate
+    
+case (MODE)
+    
+    2'b00: begin : GEN_DEC2X4
+        decoder2x4 de1 ();
+    end
+
+    
+    2'b01: begin : GEN_DEC3x8
+        decoder3x8 de2 ();
+    end
+
+    
+    2'b10: begin : GEN_DEC4x16
+        decoder4x16 de3 ();
+    end
+    default: begin : GEN_DEFAULT
+        decoder2x4 de1();
+    end
+endcase
+endgenerate
+
+
+
 
 /*
 Exercise 5 - Generate Case
@@ -21,3 +51,7 @@ depending on a parameter.
 */
 
 endmodule
+
+
+
+
